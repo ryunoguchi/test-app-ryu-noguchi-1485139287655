fc77a934d0533bb0f1276c0f2bd793338c9107f5
AB830273 <AB830273@AB830273.dhcp.hokkaido.japan.ibm.com>
2017-01-23 14:43:01 +0900

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+//var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
@@ -19,28 +19,36 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+//var appEnv = cfenv.getAppEnv();
 /*** add start***/
 // POST パラメータ取得用 body-parser設定
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true}));
-app.user(bodyParser.json());
+app.use(bodyParser.json());
 
 // Date()で現在時刻を取得する為のユーティリティ
-var dateutil = require('date-util');
+var dateutil = require('date-utils');
 
 app.post('/', function(req,res){
   var date = new Date();
   var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
   req.body.date = now;
-  
+
   console.log('app.js req.body: ' + JSON.stringify(req.body));
   res.send(req.body);
 });
 /*** add end***/
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 3000);
+//Start server
+app.listen(port, host);
+console.log('App started on port ' + port);
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+//app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+//  console.log("server starting on " + appEnv.url);
+//}
+//);
